1b3c8ba33b79f1ca31151278f3f110e6b09c2397
X140Yu <zhaoxinyu1994@gmail.com>
2017-08-26 19:57:46 +0800

    Finish header

--- src/2/index.js
+++ src/2/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import './style.css'
+import iOSBadge from './imgs/badge-app-store.png'
+import androidBadge from './imgs/badge-google-play.png'
+import heroIcons from './imgs/hero-icons.png'
+import heroiPhone from './imgs/hero-iPhone.png'
 
 class StaticPage2 extends Component {
   state = {  }
@@ -8,18 +12,25 @@ class StaticPage2 extends Component {
       <div className="StaticPage2">
         <div className="hero">
           <header>
-          <h1>Chat App</h1>
+          <h1 id="logo">ChatApp</h1>
             <ul>
               <li>Feature</li>
-              <li>How it work</li>
+              <li>How it Work</li>
               <li>Download</li>
             </ul>
           </header>
-          <h1>New Era of Messaging Secure System Messaging</h1>
-          <h2>Chat app messaging and powerful than any other application. you can enjoy free call and free chat anytime and forever</h2>
-          <img src="" alt="" />
-          <img src="" alt="" />
-          <img src="" alt="" />
+          <body>
+            <div className="description">
+              <h1>New Era of Messaging Secure System Messaging</h1>
+              <h2>Chat app messaging and powerful than any other application. you can enjoy free call and free chat anytime and forever</h2>
+              <img src={iOSBadge} alt="iOS Badge" />
+              <img src={androidBadge} alt="Android Badge" />
+            </div>
+            <div className="description-iPhone">
+              <img src={heroIcons} alt="" />
+              <img src={heroiPhone} alt="" />
+            </div>
+          </body>
         </div>
       </div>
     )
