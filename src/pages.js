84d9f437248309c3277ec9e395aada9b96830e16
X140Yu <zhaoxinyu1994@gmail.com>
2017-08-27 21:24:30 +0800

    Fix build issue

--- src/pages.js
+++ src/pages.js
@@ -1,15 +1,10 @@
-import StaticPage1 from './1'
-import StaticPage2 from './2'
-import FreeStyle from './freestyle'
 
 const pages = [
   {
-    component: StaticPage1,
     name: "airbnb landing page",
     link: "https://www.sketchappsources.com/free-source/2713-airbnb-landing-page-sketch-freebie-resource.html"
   },
   {
-    component: StaticPage2,
     name: "chat app landing page",
     link: "https://www.sketchappsources.com/free-source/2770-chat-app-landing-page-sketch-freebie-resource.html",
   },
